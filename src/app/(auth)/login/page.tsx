ffe8a92ef8e53d9d0b0d7703e977dd999e2945f9
Adebayo Ade <vanbayor@gmail.com>
2024-02-29 16:28:02 +0100

    bug fixes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useState } from 'react';
 import LoginForm from './components/LoginForm';
